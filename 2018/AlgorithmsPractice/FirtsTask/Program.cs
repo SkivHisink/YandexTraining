e3f7534eb4627cdce4790cfee9c8a89d95094cd4
khorunzhenko <49669372+SkivHisink@users.noreply.github.com>
2022-10-08 14:47:20 +0700

    readme added

--- 2018/AlgorithmsPractice/FirtsTask/Program.cs
+++ 2018/AlgorithmsPractice/FirtsTask/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirtsTask
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
